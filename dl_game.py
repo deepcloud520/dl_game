a896112736d8d6bbcce1bd1dc6baf523b9db7e9b
deepcloud520 <51439278+deepcloud520@users.noreply.github.com>
2019-10-18 20:06:34 +0800

    dl_game.py
    a frame.

--- dl_game.py
+++ dl_game.py
@@ -1,7 +1,20 @@
+'''
+=================
+= dl game frame =
+= by deepcloud  =
+=      ver 0.01 =
+=================
+'''
 import random as rd
+
 def die(nb=6):
+    '''
+    Return a random int for 1 to nb
+    '''
     return rd.randint(1,nb)
+
 class kapai:
+    '''kapai class'''
     def __init__(self,name,info,owner,**ksx):
         self.name=name
         self.info=info
@@ -20,19 +33,23 @@ class penson:
         self.name=name
         self.passkp=[]
         self.kps=kps
+        self.maxkp=3
         self.sxs=sxs
     def get(self,name,de=0):return self.sxs.get(name,de)
     def set(self,name,value):self.sxs.update({name:value})
     def calcsxs(self,name,value):
         self.set(name,self.get(name)-value)
-    def getatt(self):
-        return self.get('att')+die(6)
     def getdev(self,att):
-        dev=self.get('dev')+die(6)-self.get('att')
+        dev=self.get('dev')+die(4)-att
         return dev if dev>=0 else 0
     def getmov(self,att):
-        mov=self.get('mov')+die(6)
+        mov=self.get('mov')+die(4)
         return att if mov<att else 0
+    def _passkp(self,kp):
+        if len(self.passkp)<self.maxkp:
+            self.passkp.append(kp)
+            return True
+        return False
     def kp_add(self,kp):
         if len(self.kps)<6:
             self.kps.append(kp)
@@ -44,22 +61,24 @@ class penson:
         if self.get('hel')>self.get('maxhel'):
             self.set('hel',self.get('maxhel'))
     def calc_xg(self):
+        info=[]
         if self.get('xg'):
-            info=[]
             for dd in self.xg:
                 if dd=='fire':
                     info.append(('fire',-1))
                     self.calcsxs('hel',1)
                     self.xg.remove('fire')
+        return info
     def clean_kp(self):
         self.passkp=[]
     def __str__(self):
-        return ' str:'+self.name+' passkp:'+str(self.passkp)+' sxs:'+str(self.sxs)
+        return 'name:'+self.name+' passkp:'+str(self.passkp)+' sxs:'+str(self.sxs)
 
 class dl_game:
-    def __init__(self,plname,kps=[kapai('火剑攻击','带火的剑！','you',hel=3,xg='fire')]):
+    def __init__(self,plname,messfun,kps=[kapai('火剑攻击','带火的剑！','you',hel=3,xg='fire')]):
         self.layer=1
         self.map=[]
+        self.messfun=messfun
         self.mons=[penson('slm',[kapai('普通攻击','','you',hel=2)],3,lev=self.layer,att=1,hel=3,maxhel=3,dev=2,mov=1)]
         self.rmft=0         #room foots
         self.kps=kps        #kapais
@@ -91,6 +110,7 @@ class dl_game:
                 self.mapend('xx')
                 xx-=1
         self.mapend('end')
+    #they are privative funtion
     def __gen_mons(self):
         gs=self.layer//6+1
         for nb in range(gs):
@@ -99,27 +119,60 @@ class dl_game:
         return rd.choice(self.kps[self.player.get('lev'):])
     def __xxroom(self):
         self.player.add_hel(1)
+    def __findkp(self,tgt,sx,owner='you'):
+        info=[]
+        i=0
+        for kp in tgt.kps:
+            if kp.owner==owner:
+                if sx in kp.ksx:
+                    info.append(i)
+            i+=1
+        return info
+    # monster AI funtion
+    def __AI(self):
+        hel=self.nowm.get('hel')
+        maxh=self.nowm.get('maxhel')
+        info=[]
+        if hel/maxh<=0.3:
+            lst=self.__findkp(self.nowm,'hel','me')
+            info=lst
+            if lst:
+                for nb in lst:
+                    self.passkp(nb)
+        else:
+            lst=self.__findkp(self.nowm,'hel')
+            info=lst
+            if lst:
+                for nb in lst:
+                    self.pass_kp(nb)
+        return info
+    
     def pass_kp(self,no):
         if self.hh=='player':
-            self.player.passkp.append(self.player.kps[no])
+            self.player._passkp(self.player.kps[no])
         else:
             if self.nowm:
-                self.nowm.passkp.append(self.player.kps[no])
-    def pass_hh(self,cl=None):
+                self.nowm._passkp(self.player.kps[no])
+    def pass_hh(self):
         if self.nowm.get('hel')<=0:
-            if nms:
+            self.nowm=''
+            if len(self.nms)>1:
+                self.player.calcsxs('gol',-self.nms.get('gol'))
                 self.nms.remove(self.nowm)
                 self.nowm=self.nms[0]
             else:
-                return 'end'
+                return False
         if self.hh=='player':
-            for kp in self.player.kps:
+            self.player.calc_xg()
+            for kp in self.player.passkp:
                 if kp.owner=='me':kp.use(self.player)
                 else:kp.use(self.nowm)
             self.player.clean_kp()
             self.hh='monter'
         else:
-            for kp in self.nowm.kps:
+            self.player.calc_xg()
+            self.messfun(self.__AI())
+            for kp in self.nowm.passkp:
                 if kp.owner=='me':kp.use(self.nowm)
                 else:kp.use(self.player)
             self.nowm.clean_kp()
@@ -151,13 +204,17 @@ class dl_game:
         return info
     def __str__(self):
         return 'layer:'+str(self.layer)+' player:'+self.player.__str__()
-m=dl_game('swwm')
+def msfun(sr):
+    print(sr)
+m=dl_game('swwm',msfun)
 m.layer=3
 m.gen_map()
 print(m.map)
-print(m.go())
-print(m.go())
-print(m)
+while m.go()!='fight':pass
 m.pass_kp(0)
 m.pass_hh()
+print(m.player)
+print(m.nowm)
+m.pass_hh()
+print(m.player)
 print(m.nowm)
